a1c1be4e5fea93b4f948a028bb8bcf347594bc78
Jack Cruden <jackcruden@gmail.com>
2019-04-04 00:21:02 +1300

    Faster message events hopefully

--- app/Events/NewMessage.php
+++ app/Events/NewMessage.php
@@ -5,13 +5,12 @@ namespace App\Events;
 use App\Message;
 use Illuminate\Broadcasting\Channel;
 use Illuminate\Broadcasting\InteractsWithSockets;
-use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcastNow;
 use Illuminate\Foundation\Events\Dispatchable;
-use Illuminate\Queue\SerializesModels;
 
-class NewMessage implements ShouldBroadcast
+class NewMessage implements ShouldBroadcastNow
 {
-    use Dispatchable, InteractsWithSockets, SerializesModels;
+    use Dispatchable, InteractsWithSockets;
 
     public $message;
 
