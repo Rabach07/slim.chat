a679343a5c76bbf4ed2760478b9910fc6365dbd6
Jack Cruden <jackcruden@gmail.com>
2019-04-08 10:43:02 +1200

    Added PropertyDefinition Nova resource

--- app/PropertyDefinition.php
+++ app/PropertyDefinition.php
@@ -6,5 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class PropertyDefinition extends Model
 {
-    //
+    protected $fillable = ['name', 'type', 'business_id'];
+
+    public function business()
+    {
+        return $this->belongsTo(Business::class);
+    }
 }
